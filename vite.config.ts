cb047268ea54831369b49c8eece6179d63554a64
marcusnadaf-007 <marcusnadaf@karunya.edu.in>
2026-07-04 15:40:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: true,
+  nitro: process.env.VERCEL ? true : false,
   tanstackStart: {
     server: {
       entry: "server",
