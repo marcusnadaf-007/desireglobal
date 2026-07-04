b88f952b19f1907b3750fd51e1de187a1f37f807
marcusnadaf-007 <marcusnadaf@karunya.edu.in>
2026-07-04 15:31:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
   tanstackStart: {
     server: {
-      entry: "server"
-    },
-  },
-  nitro: {
-    preset: "vercel",
-    output: {
-      dir: ".vercel/output",
-      serverDir: ".vercel/output/functions/__server.func",
-      publicDir: ".vercel/output/static",
+      entry: "server",
     },
   },
 });
